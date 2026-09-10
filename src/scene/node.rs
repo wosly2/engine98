e3403e7aa3e213004e091f9210b8bedcd1429542
sawyer jackson <115191613+wosly2@users.noreply.github.com>
2026-09-10 11:37:57 -0400

    fixing arena stuff

--- src/scene/node.rs
+++ src/scene/node.rs
@@ -1,37 +1,20 @@
 use std::any::Any;
 
-use crate::util::arena::{Arena, IDu64};
+use crate::util::arena::{Arena, ArenaContext, IDu64};
 
 pub struct Node<T> {
-    pub value: Box<T>,
+    pub value: T,
     pub parent: Option<NodeID>,
     pub children: Vec<NodeID>,
 }
 
-type AnyNode = Node<Box<dyn Any>>;
+pub type AnyNode = Node<Box<dyn Any>>;
 
-type NodeID = IDu64;
+pub type NodeID = IDu64;
 
-type NodeTree = Arena<NodeID, AnyNode>;
+pub type NodeTree = Arena<NodeID, AnyNode>;
 
-pub struct NodeContext<'a> {
-    tree: &'a NodeTree,
-    id: NodeID,
-}
-
-impl NodeID {
-    pub fn inside(self, tree: &NodeTree) -> Option<&AnyNode> {
-        tree.get(self)
-    }
-
-    pub fn context<'a>(self, tree: &'a NodeTree) -> NodeContext<'a> {
-        NodeContext { tree, id: self }
-    }
-
-    pub fn inside_mut(self, tree: &mut NodeTree) -> Option<&mut AnyNode> {
-        tree.get_mut(self)
-    }
-}
+pub type NodeContext<'a, ID, Value> = ArenaContext<'a, ID, Value>;
 
 impl NodeTree {
     pub fn parent_id(&self, child: NodeID) -> Option<NodeID> {
@@ -43,14 +26,8 @@ impl NodeTree {
     }
 
     pub fn add_child(&mut self, parent: NodeID, child: NodeID) -> Option<()> {
-        parent.inside_mut(self)?.children.push(child);
+        self.get_mut(parent)?.children.push(child);
 
         Some(())
-    }
-}
-
-impl<'a> NodeContext<'a> {
-    pub fn node(self) -> Option<&'a AnyNode> {
-        self.tree.get(self.id)
     }
 }
--- src/util/arena.rs
+++ src/util/arena.rs
@@ -1,46 +1,44 @@
 use std::{collections::HashMap, hash::Hash};
 
-pub struct Arena<ID: ArenaID<Value>, Value> {
+pub struct Arena<ID: ArenaID, Value> {
     pub values: HashMap<ID, Value>,
     next_id: ID,
 }
 
-pub struct ArenaContext<'a, ID: ArenaID<Value>, Value> {
+pub struct ArenaContext<'a, ID: ArenaID, Value> {
     pub arena: &'a Arena<ID, Value>,
     pub id: ID,
 }
 
-impl<'a, ID: ArenaID<Value>, Value> ArenaContext<'a, ID, Value> {
+impl<'a, ID: ArenaID, Value> ArenaContext<'a, ID, Value> {
     pub fn value(self) -> Option<&'a Value> {
         self.arena.get(self.id)
     }
 }
 
-pub trait ArenaID<Value>: Eq + Hash + Copy + Clone {
+pub trait ArenaID: Eq + Hash + Copy + Clone {
     fn generate_next(&self) -> Self;
 
-    fn inside(self, arena: &Arena<Self, Value>) -> Option<&Value> {
+    fn inside<Value>(self, arena: &Arena<Self, Value>) -> Option<&Value> {
         arena.get(self)
     }
 
-    fn context<'a>(self, arena: &'a Arena<Self, Value>) -> ArenaContext<'a, Self, Value> {
+    fn context<'a, Value>(self, arena: &'a Arena<Self, Value>) -> ArenaContext<'a, Self, Value> {
         ArenaContext { arena, id: self }
     }
 
-    fn inside_mut(self, tree: &mut Arena<Self, Value>) -> Option<&mut Value> {
-        tree.get_mut(self)
+    fn inside_mut<Value>(self, arena: &mut Arena<Self, Value>) -> Option<&mut Value> {
+        arena.get_mut(self)
     }
 }
 
-impl<ID: ArenaID<Value>, Value> Arena<ID, Value> {
+impl<ID: ArenaID, Value> Arena<ID, Value> {
+    // simplisitic for now
     pub fn generate_id(&mut self) -> ID {
-        loop {
-            if let Some(_) = self.get(self.next_id) {
-                self.next_id.generate_next();
-            } else {
-                return self.next_id;
-            }
+        while let Some(_) = self.get(self.next_id) {
+            self.next_id = self.next_id.generate_next();
         }
+        self.next_id
     }
 
     pub fn get(&self, value: ID) -> Option<&Value> {
@@ -51,19 +49,23 @@ impl<ID: ArenaID<Value>, Value> Arena<ID, Value> {
         self.values.get_mut(&value)
     }
 
-    pub fn add_value(&mut self, value: Value) -> Option<ID> {
+    pub fn push(&mut self, value: Value) -> ID {
         let id = self.generate_id();
-        self.values.insert(id, value)?;
+        self.values.insert(id, value);
+        id
+    }
 
-        Some(id)
+    pub fn pop(&mut self, id: ID) -> Option<Value> {
+        self.values.remove(&id)
     }
 }
 
 #[derive(Clone, Copy, Hash, Eq, PartialEq)]
 pub struct IDu64(u64);
 
-impl<Value> ArenaID<Value> for IDu64 {
+impl ArenaID for IDu64 {
     fn generate_next(&self) -> Self {
+        // very very simplistic
         IDu64(self.0 + 1)
     }
 }
